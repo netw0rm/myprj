287f053f36905112faa5649b40b707510e91db6f
shawn <qiusct@gmail.com>
2017-07-05 14:42:59 +0800

    init myprj

---
+++ home/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+
+url.patterns = [
+    #url(r'^$', home.index(), name='home'),
+]
---
+++ rest/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest.serializers import UserSerializer, GroupSerializer
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
